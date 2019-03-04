4cd77d7e944b50868b2ab3304bc4eec9881869b8
Marco Lombardini <marco.lombardini@gmail.com>
2019-03-04 15:22:13 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -3,9 +3,9 @@
 def speak_to_grandma(phrase = "HUH?! SPEAK UP, SONNY!")
   if phrase == "I LOVE YOU GRANDMA!"
     puts "I LOVE YOU TOO PUMPKIN!"
-  elsif
+  elsif phrase.upcase
     puts "NO, NOT SINCE 1938!"
-  else == phrase
+  else == phrase.downcase
   end
 end
 
